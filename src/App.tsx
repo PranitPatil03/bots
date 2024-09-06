7a62620cb9a32fc01c0e764b9b6041a01cfc3940
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-09-06 13:37:42 +0530

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Layout from "./page/Layout";
 import HomePage from "./page/Home";
-import Dashboard from "./components/Dashboard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="dashboard" element={<Dashboard />} />
         </Route>
       </Routes>
     </Router>
